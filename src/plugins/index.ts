c9be74e554692881e7846bfa93e78c6e42ef1234
Iris Mittelbach <iris.mittelbach@gmx.de>
2023-11-20 03:03:09 +0100

    remove unused pinia

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -6,13 +6,12 @@
 
 // Plugins
 import vuetify from './vuetify'
-import pinia from '../store'
+
 
 // Types
 import type { App } from 'vue'
 
-export function registerPlugins (app: App) {
+export function registerPlugins(app: App) {
   app
     .use(vuetify)
-    .use(pinia)
 }
